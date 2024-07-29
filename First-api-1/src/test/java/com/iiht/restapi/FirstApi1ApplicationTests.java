3dd7a8f86bfc6f4b6be74cbe8dc7aa0d778a0561
unknown <https://github.com/nsj2024/MavenProject2.git>
2024-07-29 17:38:25 +0530

    Initial commit

---
+++ First-api-1/src/test/java/com/iiht/restapi/FirstApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiht.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
